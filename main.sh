367657a14371b7f119ccf28e6747e50242d14eb9
b2334sy <b2334sy@aiit.ac.jp>
2023-07-30 00:59:34 +0000

    main, test修正

--- main.sh
+++ main.sh
@@ -1,7 +1,37 @@
 #!/usr/bin/bash
 
 # 入力数
+if [ $# -ne 2 ]; then
+        echo "引数は2つの数値を指定してください。"
+        exit 1
+fi
 
-# 入力が正の自然数
+#　入力が正の自然数
+if ! [[ "$1" =~ ^[1-9][0-9]*$ ]] || ! [[ "$2" =~ ^[1-9][0-9]*$ ]]; then
+        echo "引数は正の自然数を指定してください。"
+        exit 1
+fi
 
 # メインの処理
+if [ $1 -lt $2 ]; then
+        num1=$2
+        num2=$1
+fi
+
+num1=$1
+num2=$2
+
+while [ $num2 -ne 0 ]; do
+        remainder=$((num1 % num2))
+        num1=$num2
+        num2=$remainder
+        if [ $num2 -eq 1 ]; then
+                break
+        fi
+done
+
+if [ $num2 -eq 1 ]; then
+        echo $num2
+else
+        echo $num1
+fi
--- test_main.sh
+++ test_main.sh
@@ -1,25 +1,73 @@
-  1 #!/usr/bin/bash
-  2
-  3 # 入力数が少ない場合
-  4 # 入力なし
-  5 # 入力1個入力
-  6 # 入力3個以上
-  7
-  8 # 自然数以外が入力された場合
-  9 # a b ->"error:plz input natural number
- 10 # -1
- 11
- 12 # 最大公約数が計算できているか
- 13 # 2 4 -> 2
- 14 echo "2" > /tmp/SS-ans
- 15 ./main 2 4 > /temp/$$-result
- 16 diff /tmp/$$-ans /tmp/$$-result || echo "テスト 3-1" >> /tmp/$$-error.log
- 17
- 18 # 1000 100 -> 100
- 19 # 12 8 -> 4
- 20
- 21 if [ -f /tmp/$$-error.log ];then
- 22         cat /tmp/$$-error.log 1>&2
- 23         rm -f /tmp/$$-*
- 24         exit 1
- 25 fi
+#!/usr/bin/bash
+
+# 入力なし
+echo "引数は2つの数値を指定してください。" > /tmp/$$-ans
+./main > /tmp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト ./main" >> /tmp/$$-error.log
+rm /tmp/$$-ans /tmp/$$-result
+
+# 入力1個
+echo "引数は2つの数値を指定してください。" > /tmp/$$-ans
+./main 3 > /tmp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト ./main 3" >> /tmp/$$-error.log
+rm /tmp/$$-ans /tmp/$$-result
+
+# 入力3個以上
+echo "引数は2つの数値を指定してください。" > /tmp/$$-ans
+./main 2 4 8 > /tmp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト ./main 2 4 8" >> /tmp/$$-error.log
+rm /tmp/$$-ans /tmp/$$-result
+
+#自然数以外が入力された場合
+# 0 10 -> ERROR
+echo "引数は正の自然数を指定してください。" > /tmp/$$-ans
+./main 0 10 > /tmp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト ./main 0 10" >> /tmp/$$-error.log
+rm /tmp/$$-ans /tmp/$$-result
+
+# -2 4 -> ERROR
+echo "引数は正の自然数を指定してください。" > /tmp/$$-ans
+./main -2 4 > /tmp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト ./main -2 4" >> /tmp/$$-error.log
+rm /tmp/$$-ans /tmp/$$-result
+
+# D 51 -> ERROR
+echo "引数は正の自然数を指定してください。" > /tmp/$$-ans
+./main D 51 > /tmp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト ./main D 51" >> /tmp/$$-error.log
+rm /tmp/$$-ans /tmp/$$-result
+
+
+# 最大公約数が計算できているか
+# 2 4 -> 2
+echo "2" > /tmp/$$-ans
+./main 2 4 > /temp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト ./main 2 4" >> /tmp/$$-error.log
+rm /tmp/$$-ans /tmp/$$-result
+
+# 1000 100 -> 100
+echo "100" > /tmp/$$-ans
+./main 1000 100 > /temp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト ./main 1000 100" >> /tmp/$$-error.log
+rm /tmp/$$-ans /tmp/$$-result
+
+# 35 48 -> 1
+echo "1" > /tmp/$$-ans
+./main 35 48 > /temp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト ./main 35 48" >> /tmp/$$-error.log
+rm /tmp/$$-ans /tmp/$$-result
+
+# エラー結果出力
+if [ -f /tmp/$$-error.log ]; then
+        cat /tmp/$$-error.log 1>&2
+        rm /tmp/$$-error.log
+        fi
+done
+
+if [ $num2 -eq 1 ]; then
+        echo $num2
+else
+        echo $num1
+fi
+        exit 1
+fi
